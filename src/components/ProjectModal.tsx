8ea53c98e33f62a2e0b5e67e9f2260ac1c1d10a9
Leonid Budkov <leonid.budkov@gmail.com>
2026-03-07 10:52:56 +0100

    feat(modal): add close on backdrop click

--- src/components/ProjectModal.tsx
+++ src/components/ProjectModal.tsx
@@ -13,8 +13,12 @@ export default function ProjectModal({
   initialStatus,
 }: ProjectModalProps) {
   return (
-    <div className="fixed inset-0 backdrop-blur-sm flex items-center justify-center">
-      <div className="bg-gray-100 rounded-2xl border-green-500 border-2 p-4 w-[28rem] max-w-lg w-full ">
+    <>
+      <div
+        className="fixed inset-0 backdrop-blur-sm flex items-center justify-center"
+        onClick={() => onClose(false)}
+      />
+      <div className="bg-gray-100 rounded-2xl border-green-500 border-2 p-4 w-[28rem] max-w-lg w-full z-10 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         {/* Header */}
         <button
           className="top-2 right-2 text-gray-500 hover:text-gray-700"
@@ -24,6 +28,6 @@ export default function ProjectModal({
         </button>
         <div>Initial Status: {initialStatus}</div>
       </div>
-    </div>
+    </>
   )
 }
