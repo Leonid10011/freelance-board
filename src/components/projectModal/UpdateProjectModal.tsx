7b01b93fa7f16c0b5aa94c059e4dc724911eb64d
Leonid Budkov <leonid.budkov@gmail.com>
2026-03-09 20:21:04 +0100

    feat(update-project-modal): add UpdateProjecModal update function and ui render and successfull submit

--- src/components/projectModal/UpdateProjectModal.tsx
+++ src/components/projectModal/UpdateProjectModal.tsx
@@ -2,13 +2,19 @@ import { Project } from "@/domain/project"
 import { useProjectForm } from "./useProjectForm"
 import { FormStateType } from "./types"
 import { format } from "date-fns"
+import ProjectModalShell from "./ProjectModalShell"
+import ProjectFormFields from "./ProjectFormFields"
+import { UpdateProjectSchema } from "@/validation/project.schema"
+import { updateProject } from "@/repo/project.repo"
 
 type UpdateProjectModalProps = {
   project: Project
+  onUpdate: (updatedProject: Project) => void
 }
 
 export default function UpdateProjectModal({
   project,
+  onUpdate,
 }: UpdateProjectModalProps) {
   const initialFormState: FormStateType = {
     title: project.title ?? "",
@@ -30,4 +36,46 @@ export default function UpdateProjectModal({
     setSubmitting,
     setError,
   } = useProjectForm({ initialState: initialFormState })
+
+  const handleUpdate = async () => {
+    if (isSubmitting) {
+      return
+    }
+
+    try {
+      setError(null)
+      setSubmitting(true)
+      const validatedData = UpdateProjectSchema.safeParse(formState)
+      if (!validatedData.success) {
+        console.error("Validation failed:", validatedData.error)
+        return
+      }
+      const result = await updateProject(project.id, validatedData.data)
+      // setSaveSuccess("Project updated successfully!") /* Temporarily disable success message to focus on error handling */
+      onUpdate(result)
+    } catch (error) {
+      console.error("Error updating project:", error)
+      setError("Failed to update project. Please try again.")
+    } finally {
+      setSubmitting(false)
+    }
+  }
+
+  return (
+    <ProjectModalShell
+      isSubmitting={isSubmitting}
+      onPrimaryAction={handleUpdate}
+      primaryActionLabel="Update"
+      onClose={() => {}}
+      errorMessage={saveError}
+    >
+      <ProjectFormFields
+        formState={formState}
+        onDeadlineChange={handleDateChange}
+        onProjectStatusChange={handleProjectStatusChange}
+        onProjectPriorityChange={handleProjectPriorityChange}
+        onTextFieldChange={handleInputChange}
+      />
+    </ProjectModalShell>
+  )
 }
