41efb41c8158cc564fc68af19c675abe70234cf4
Leonid Budkov <leonid.budkov@gmail.com>
2026-03-11 10:10:08 +0100

    fix(board-page): applied repsonsivness to non demo board

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -7,8 +7,13 @@ export default async function Home() {
   const initialProjects: Project[] = await getProjects()
 
   return (
-    <>
-      <Board initialProjects={initialProjects} />
-    </>
+    <div className="min-h-screen">
+      <div className="mx-auto w-full max-w-[1600px] px-4 py-6 md:px-4 md:py-8">
+        <h1 className="text-2xl font-bold">DEMO Freelance Board</h1>
+        <div className="mt-4">
+          <Board initialProjects={initialProjects} />
+        </div>
+      </div>
+    </div>
   )
 }
